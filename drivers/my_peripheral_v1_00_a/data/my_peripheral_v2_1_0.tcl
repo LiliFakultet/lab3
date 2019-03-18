9ae1b1a3f4dc8ac40c1a1a081e433b78a1ac6c71
Denis Liliom <thvaarff@gmail.com>
2019-03-18 12:06:52 +0100

    Finished my_peripheral

---
+++ drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
@@ -0,0 +1,11 @@
+##############################################################################
+## Filename:          D:\RA31-2016\LPRS2\lab3/drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
+## Description:       Microprocess Driver Command (tcl)
+## Date:              Mon Mar 18 11:30:03 2019 (by Create and Import Peripheral Wizard)
+##############################################################################
+
+#uses "xillib.tcl"
+
+proc generate {drv_handle} {
+  xdefine_include_file $drv_handle "xparameters.h" "my_peripheral" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" 
+}
---
+++ drivers/my_peripheral_v1_00_a/src/my_peripheral.c
@@ -0,0 +1,17 @@
+/*****************************************************************************
+* Filename:          D:\RA31-2016\LPRS2\lab3/drivers/my_peripheral_v1_00_a/src/my_peripheral.c
+* Version:           1.00.a
+* Description:       my_peripheral Driver Source File
+* Date:              Mon Mar 18 11:30:03 2019 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "my_peripheral.h"
+
+/************************** Function Definitions ***************************/
+
+void MY_PERIPHERAL_ClearScreen(){
+	xil_printf("%c[2J",27);
+}
